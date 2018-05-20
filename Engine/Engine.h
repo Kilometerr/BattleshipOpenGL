9aa78e1b7d702734b58ed15db40bace335314394
Kilometerr <tymoteusz128@gmail.com>
2018-05-20 03:00:19 +0200

    Create Engine.h

---
+++ Engine/Engine.h
@@ -0,0 +1,30 @@
+#ifndef AZ_ENGINE
+#define AZ_ENGINE
+
+#include "GLFW\glfw3.h"
+#pragma comment(lib,"opengl32.lib")
+
+#include <iostream>
+using namespace std;
+
+class Engine
+{
+public:
+	static int SCREEN_WIDTH;
+	static int SCREEN_HEIGHT;
+
+
+	Engine();
+	~Engine();
+
+	bool Initialize(char* windowTitle);
+
+	void Update();
+	void Render();
+
+private:
+	static GLFWwindow* window;
+};
+
+
+#endif 
